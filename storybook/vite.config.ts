4eeb9823bfa49527bc053ec9de83f27d946fec32
IlYeong Jeong <developjiy@gmail.com>
2023-08-28 21:34:22 +0900

    ref #6 기본 패키지 구성 - 스타일 라이브러리 셋팅

--- storybook/vite.config.ts
+++ storybook/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import macrosPlugin from 'vite-plugin-babel-macros';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: 5501,
+    host: '0.0.0.0',
+  },
+  plugins: [
+    react(),
+    macrosPlugin()
+  ],
 })
