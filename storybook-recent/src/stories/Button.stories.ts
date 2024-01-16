35664aac5c4be65f0434eccfd34cf73145534a0f
IlYeong Jeong <developjiy@gmail.com>
2024-01-16 23:34:06 +0900

    ref #6 storybook 버튼 컴포넌트 예시 추가

--- storybook-recent/src/stories/Button.stories.ts
+++ storybook-recent/src/stories/Button.stories.ts
@@ -1,50 +1,57 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Button } from './Button';
+import Button, { ButtonColor, ButtonSize } from './Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'Example/Button',
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  args: {
+    children: 'Button',
+  },
+};
+
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'Button',
+    color: ButtonColor.PRIMARY,
+    children: 'Button',
   },
 };
 
-export const Secondary: Story = {
+export const Dark: Story = {
   args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
+    color: ButtonColor.DARK,
+    children: 'Button',
   },
 };
 
 export const Small: Story = {
   args: {
-    size: 'small',
-    label: 'Button',
+    size: ButtonSize.SMALL,
+    children: 'Button',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: ButtonSize.LARGE,
+    children: 'Button',
   },
 };
